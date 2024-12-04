97b17f2d171d55d9d5ae86ee92085f9ce48f9ca6
JoseLuisHcoder <zonajl@gmail.com>
2024-12-04 18:18:30 -0400

    add screen init

--- lib/config/theme/app_colors.dart
+++ lib/config/theme/app_colors.dart
@@ -5,4 +5,5 @@ class AppColors {
   static const Color secondaryColor = Color(0xFFD9D9D9);
   static const Color background = Colors.grey;
   static const Color accentColor = Colors.white;
+  static const Color blueColor = Color(0xFF006078);
 }
--- lib/presentation/screens/init/init_screen.dart
+++ lib/presentation/screens/init/init_screen.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:h3_14_bookie/config/theme/app_colors.dart';
 import 'package:h3_14_bookie/domain/services/auth_service.dart';
+import 'package:h3_14_bookie/presentation/screens/login/login.dart';
 import 'package:h3_14_bookie/presentation/screens/signup/signup.dart';
 
-class Login extends StatelessWidget {
-  Login({super.key});
-  static const String name = 'Login';
+class InitScreen extends StatelessWidget {
+  InitScreen({super.key});
+  static const String name = 'InitScreen';
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
@@ -24,38 +25,38 @@ class Login extends StatelessWidget {
           onPressed: () => Navigator.pop(context),
         ),
       ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 20),
-              Text(
-                'Inicio de sesión',
-                style: GoogleFonts.inter(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                ),
+      body: Stack(
+        children: [
+          // Fondo de imagen
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                    'assets/images/background.jpg'), // Ruta de tu imagen
+                fit: BoxFit
+                    .cover, // Asegura que la imagen cubra toda la pantalla
               ),
-              const SizedBox(height: 8),
-              Text(
-                'Para comenzar, ingresa a tu cuenta.',
-                style: GoogleFonts.inter(
-                  fontSize: 14,
-                  color: Colors.black87,
-                ),
-              ),
-              const SizedBox(height: 32),
-              _socialLoginButtons(),
-              const SizedBox(height: 24),
-              _divider(),
-              const SizedBox(height: 24),
-              _loginForm(context),
-            ],
+            ),
           ),
-        ),
+          // Contenido principal
+          SafeArea(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 20),
+                  const SizedBox(height: 32),
+                  _socialLoginButtons(),
+                  const SizedBox(height: 24),
+                  _divider(),
+                  const SizedBox(height: 24),
+                  _loginForm(context),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -115,46 +116,7 @@ class Login extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        TextField(
-          controller: _emailController,
-          decoration: InputDecoration(
-            hintText: 'Usuario o correo electrónico',
-            filled: true,
-            fillColor: Colors.grey.shade100,
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-              borderSide: BorderSide.none,
-            ),
-            contentPadding: const EdgeInsets.all(16),
-          ),
-        ),
         const SizedBox(height: 16),
-        TextField(
-          controller: _passwordController,
-          obscureText: true,
-          decoration: InputDecoration(
-            hintText: 'Contraseña',
-            filled: true,
-            fillColor: Colors.grey.shade100,
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-              borderSide: BorderSide.none,
-            ),
-            contentPadding: const EdgeInsets.all(16),
-            suffixIcon: IconButton(
-              icon: const Icon(Icons.visibility_off),
-              onPressed: () {},
-            ),
-          ),
-        ),
-        const SizedBox(height: 16),
-        TextButton(
-          onPressed: () {},
-          child: Text(
-            '¿Olvidaste tu contraseña?',
-            style: TextStyle(color: Colors.blue.shade700),
-          ),
-        ),
         const SizedBox(height: 16),
         ElevatedButton(
           style: ElevatedButton.styleFrom(
@@ -164,16 +126,9 @@ class Login extends StatelessWidget {
             ),
             padding: const EdgeInsets.symmetric(vertical: 16),
           ),
-          onPressed: () async {
-            try {
-              await AuthService().signin(
-                email: _emailController.text,
-                password: _passwordController.text,
-                context: context,
-              );
-            } catch (e) {
-              // Manejar el error
-            }
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Login()));
           },
           child: const Text(
             'Iniciar sesión',
@@ -181,22 +136,29 @@ class Login extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 16),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: AppColors.blueColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(28),
+            ),
+            padding: const EdgeInsets.symmetric(vertical: 16),
+          ),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Signup()));
+          },
+          child: const Text(
+            'Registrarme',
+            style: TextStyle(fontSize: 16, color: AppColors.accentColor),
+          ),
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
-              '¿Aún no tienes tu cuenta? ',
-              style: TextStyle(color: Colors.black87),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => Signup()));
-              },
-              child: Text(
-                'Regístrate',
-                style: TextStyle(color: Colors.blue.shade700),
-              ),
+              'Al registrarte, aceptas nuestros Terminos de uso y Politicas de Privacidad',
+              style: TextStyle(color: AppColors.background),
             ),
           ],
         ),
