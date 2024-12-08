102c15d5ed3f9bbf60be4915a4ab15e328796dcf
Daniel Matos <deme12.1@gmail.com>
2024-12-08 07:06:39 -0500

    Test for chapters creation

--- lib/presentation/views/home/favorites_view.dart
+++ lib/presentation/views/home/favorites_view.dart
@@ -1,170 +1,159 @@
 import 'package:flutter/material.dart';
-import 'package:h3_14_bookie/presentation/widgets/home/book_widget.dart';
+import 'package:h3_14_bookie/domain/model/app_user.dart';
+import 'package:h3_14_bookie/domain/model/dto/category_dto.dart';
+import 'package:h3_14_bookie/domain/model/dto/chapter_dto.dart';
+import 'package:h3_14_bookie/domain/model/dto/story_dto.dart';
+import 'package:h3_14_bookie/domain/model/dto/user_dto.dart';
+import 'package:h3_14_bookie/domain/model/reading.dart';
+import 'package:h3_14_bookie/domain/model/story.dart';
+import 'package:h3_14_bookie/domain/model/writing.dart';
+import 'package:h3_14_bookie/domain/services/app_user_service.dart';
+import 'package:h3_14_bookie/domain/services/auth_service.dart';
+import 'package:h3_14_bookie/domain/services/category_service.dart';
+import 'package:h3_14_bookie/domain/services/chapter_service.dart';
+import 'package:h3_14_bookie/domain/services/firebase_service.dart';
+import 'package:h3_14_bookie/domain/services/implement/app_user_service_impl.dart';
+import 'package:h3_14_bookie/domain/services/implement/category_service_impl.dart';
+import 'package:h3_14_bookie/domain/services/implement/chapter_service_impl.dart';
+import 'package:h3_14_bookie/domain/services/implement/story_service_impl.dart';
+import 'package:h3_14_bookie/domain/services/story_service.dart';
 
 class FavoritesView extends StatelessWidget {
   const FavoritesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 40), // Espaciado entre AppBar y encabezado
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 28),
-              child: Text(
-                'Continuar leyendo',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                  fontFamily: 'Roboto',
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            SizedBox(
-              height: 290,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(left: 16),
-                    child: ContinuarLeyendoCard(),
-                  );
-                },
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 28),
-              child: Text(
-                'Tu colección',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                  fontFamily: 'Roboto',
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: GridView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: 10,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                  childAspectRatio: 180 / 290,
-                ),
-                itemBuilder: (context, index) {
-                  return const BookWidget();
-                },
-              ),
-            ),
-          ],
+    return Column(
+      children: [
+        const SizedBox(height: 100),
+        const Center(
+          child: Text('Favorites View'),
         ),
-      );
+        const SizedBox(height: 50),
+        _Button(context),
+        _Button2(context),
+      ],
+    );
   }
 }
 
-class ContinuarLeyendoCard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 180,
-      height: 290,
-      margin: const EdgeInsets.only(bottom: 20),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: const [
-          BoxShadow(
-            color: Color(0x3F000000),
-            blurRadius: 8,
-            offset: Offset(0, 4),
+Widget _Button(BuildContext context) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      const SizedBox(height: 16),
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xFF006494),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
           ),
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(13),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: 140,
-              height: 140,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                image: const DecorationImage(
-                  image: NetworkImage("https://via.placeholder.com/140"),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            const SizedBox(height: 5),
-            const Text(
-              'La Mansión de los...',
-              style: TextStyle(
-                fontSize: 16,
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
-            const SizedBox(height: 4),
-            const Text(
-              'En un pueblo olvidado por el tiempo, una jove...',
-              style: TextStyle(
-                fontSize: 14,
-                fontFamily: 'Roboto',
-                color: Colors.black,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Stack(
-              children: [
-                Container(
-                  height: 5,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFDADADA),
-                    borderRadius: BorderRadius.circular(28),
-                  ),
-                ),
-                Container(
-                  height: 5,
-                  width: 100,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF006078),
-                    borderRadius: BorderRadius.circular(28),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            const Row(
-              children: [
-                Icon(Icons.book, size: 24),
-                SizedBox(width: 5),
-                Text(
-                  '13 Capítulos',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ],
-            ),
-          ],
+          minimumSize: const Size(double.infinity, 56),
+          elevation: 0,
+        ),
+        onPressed: () async {
+          IChapterService chapterService = ChapterServiceImpl();
+          ChapterDto chapterDto = ChapterDto(
+            storyUid: 'fpoUZ07nsat0zB11ppfQ',
+            title: 'Chapter 2',
+            pages: [
+              'lorem ipsum dolor sit amet',
+              'consectetur adipiscing elit',
+              'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+            ],
+            placeName: 'Place 2',
+            lat: 134,
+            long: 111,
+          );
+          final chapterUid = await chapterService.createChapter(chapterDto);
+          print(chapterUid);
+        },
+        child: const Text(
+          "crear",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
         ),
       ),
-    );
-  }
+    ],
+  );
+}
+
+Widget _CategoryList(List<CategoryDto> categories) {
+  return ListView.builder(
+    shrinkWrap: true,
+    itemCount: categories.length,
+    itemBuilder: (context, index) {
+      final category = categories[index];
+      return Card(
+        margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+        child: ListTile(
+          title: Text(category.name),
+          subtitle: Text('ID: ${category.uid}'),
+        ),
+      );
+    },
+  );
+}
+
+Widget _Button2(BuildContext context) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      const SizedBox(height: 16),
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xFF006494),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          minimumSize: const Size(double.infinity, 56),
+          elevation: 0,
+        ),
+        onPressed: () async {
+          IStoryService storyService = StoryServiceImpl();
+          IChapterService chapterService = ChapterServiceImpl();
+          ICategoryService categoryService = CategoryServiceImpl();
+          final categories = await categoryService.getCategories();
+          print(categories);
+          String categoryUid = '';
+          for (var category in categories) {
+            categoryUid = category.uid;
+          }
+          print(categoryUid);
+
+          final storyDto = StoryDto(
+              title: 'Story 1',
+              cover: 'cover1.jpg',
+              synopsis: 'This is a test story',
+              categoriesUid: [categoryUid],
+              labels: ['asdf', 'afasd']);
+          final storyUid = await storyService.createStory(storyDto);
+          ChapterDto chapterDto = ChapterDto(
+            storyUid: storyUid,
+            title: 'Chapter 1',
+            pages: [
+              'lorem ipsum dolor sit amet',
+              'consectetur adipiscing elit'
+            ],
+            placeName: 'Place 1',
+            lat: 123,
+            long: 456,
+          );
+          final chapterUid = await chapterService.createChapter(chapterDto);
+          print(chapterUid);
+        },
+        child: const Text(
+          "Actualizar datos",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    ],
+  );
 }
